5a4bef48ad0dd8146308c1ddbce523a0074ce393
Princeton45 <princetonabdulsalam25@gmail.com>
2017-07-07 12:52:47 -0400

    Add files via upload

---
+++ age.js
@@ -0,0 +1,5 @@
+var age = prompt("How old are you?")
+var yearsOld = "yearsold"
+var days = age * 365;
+alert(age + " years is roughly " + days + " days")
+console.log("You are, " + age + " " + yearsOld)
